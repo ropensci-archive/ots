4a8acfeb011fbdb78d3291d8b2e7013f243cdab3
Scott Chamberlain <myrmecocystus@gmail.com>
2015-07-06 08:24:45 -0700

    updated man files for new roxygen2, and importing all non-base R fxns from methods and utils pkgs

--- R/ots-package.R
+++ R/ots-package.R
@@ -1,5 +1,7 @@
 #' ots, an R client for various ocean time series datasets.
 #'
+#' @importFrom methods is
+#' @importFrom utils untar read.table read.csv head
 #' @name ots-package
 #' @aliases ots
 #' @docType package
