692cc351499914a43b97cc0aa6e16b6c28456047
Scott Chamberlain <myrmecocystus@gmail.com>
2016-01-17 10:50:56 -0800

    importFrom stats setnames

--- R/ots-package.R
+++ R/ots-package.R
@@ -1,6 +1,7 @@
 #' ots, an R client for various ocean time series datasets.
 #'
 #' @importFrom methods is
+#' @importFrom stats setNames
 #' @importFrom utils untar read.table read.csv head
 #' @importFrom httr GET content write_disk stop_for_status
 #' @importFrom curl curl_download
